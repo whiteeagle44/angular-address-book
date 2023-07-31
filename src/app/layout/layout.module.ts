1d41fed4317fb964d74d01112913dcacc3e8aae3
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-07-31 14:23:28 +0200

    Fix module declarations

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,9 +6,10 @@ import {MenuComponent} from './menu/menu.component';
 import {ContactsListComponent} from './contacts-list/contacts-list.component';
 import {AddContactComponent} from './add-contact/add-contact.component';
 import {ContactEditComponent} from "./contact-edit/contact-edit.component";
+import {ContactViewComponent} from "./contact-view/contact-view.component";
 
 @NgModule({
-  declarations: [MenuComponent, ContactsListComponent, AddContactComponent, ContactEditComponent],
+  declarations: [MenuComponent, ContactsListComponent, AddContactComponent, ContactViewComponent, ContactEditComponent],
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
   exports: [MenuComponent],
 })
