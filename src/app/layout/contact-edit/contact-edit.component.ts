db2f6138d8169d50b621e004611cadbfd53bdfcc
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-07-31 13:44:43 +0200

    Initial version of contact-edit component

--- src/app/layout/contact-edit/contact-edit.component.ts
+++ src/app/layout/contact-edit/contact-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Contact, ContactService} from "../../contacts/contact.service";
 import {Observable, Subscription} from "rxjs";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-edit',
@@ -9,23 +10,39 @@ import {Observable, Subscription} from "rxjs";
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit, OnDestroy {
-  sub?: Subscription;
+  subs: Subscription[] = [];
+  id$?: number;
   contact$?: Observable<Contact | undefined>;
+  contactForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    street: ['', Validators.required],
+    city: ['', Validators.required]
+  });
 
   constructor(
     private route: ActivatedRoute,
-    private contactService: ContactService
+    private contactService: ContactService,
+    private formBuilder: FormBuilder
   ) {
   }
 
+  submit(e: Event) {
+    e.preventDefault();
+    if (!this.id$) return;
+    const sub = this.contactService.updateContactById(this.id$, this.contactForm.value as Partial<Contact>).subscribe();
+    this.subs.push(sub);
+  }
+
   ngOnInit(): void {
-    this.sub = this.route.params.subscribe((params) => {
-      const id = +params['contactId']
-      this.contact$ = this.contactService.getContactById(id)
+    const sub = this.route.params.subscribe((params) => {
+      this.id$ = +params['contactId']
+      this.contact$ = this.contactService.getContactById(this.id$)
     })
+    this.subs.push(sub);
   }
 
   ngOnDestroy(): void {
-    this.sub?.unsubscribe()
+    this.subs.forEach(s => s.unsubscribe());
   }
 }
